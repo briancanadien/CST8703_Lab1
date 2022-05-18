ab76e457e98a21209ebd23f6396d846d25432fd1
Kyle Chisholm <chishok@algonquincollege.com>
2022-05-18 00:00:30 -0400

    Lab0: auto update

--- scripts/run_submission.sh
+++ scripts/run_submission.sh
@@ -7,10 +7,13 @@ rm -rf ./build
 cmake -DCMAKE_INSTALL_PREFIX=${HOME}/.local -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S. -B./build
 cmake --build ./build --target ${TARGET_NAME} -j 4 --
 
-./build/${SUBFOLDER}/${TARGET_NAME} |& tee output.txt &
+./build/${SUBFOLDER}/${TARGET_NAME} |& tee output_user.txt &
 
 HELLO_PID=$(pgrep -f ${TARGET_NAME})
-ps -F -p ${HELLO_PID} |& tee -a output.txt
+ps -F -p ${HELLO_PID} |& tee -a output_user.txt
+# after 4 seconds, signal will be sent anyways
 read -t 4 -p "Press any key to send TERM signal to PID ${HELLO_PID} ..."
 kill -s TERM ${HELLO_PID}
 echo "Signalled."
+
+sudo ./build/${SUBFOLDER}/${TARGET_NAME} | tee output_root.txt
